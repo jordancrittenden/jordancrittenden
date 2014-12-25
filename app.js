d99d0e2e3a135ab7b552cf4fad95ced1ee321fc6
Jordan Crittenden <jordan.s.crittenden@gmail.com>
2014-12-24 21:43:52 -0700

    Initial import

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+app.engine('.html', require('ejs').__express);
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/img/'));
+app.use(express.static(__dirname + '/font/'));
+app.set('views', __dirname + '/');
+
+app.get('/', function (req, res) {
+	res.render('resume.html');
+});
+
+
+app.listen(process.env.PORT || 3000);
